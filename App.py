3cf7f666f0d0cbaebba485be5d9a1356d3f2d196
Алексей Гомза <gomzik000@gmail.com>
2012-05-07 01:14:55 +0300

    Added functionality to save config with cookies and path to download

--- App.py
+++ App.py
@@ -2,7 +2,9 @@
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from PyQt4.QtNetwork import *
 import sys
+import os
 from Login import Login
 from Settings import Settings
 from SyncThread import SyncThread
@@ -14,9 +16,30 @@ class App(QObject):
     
     def __init__(self, parent = None):
         QObject.__init__(self, parent)
+        
+        self.config = {}
+        self.config['browser'] = {}
+        self.config['browser']['cookies'] = {}
+        self.config['path'] = ''
+        if os.path.exists('.config'):
+            config = open('.config', 'rb')
+            self.config = pickle.loads(config.read())
+            print self.config
+            config.close()
+        
         self.tray = QSystemTrayIcon(QIcon('images/default_icon.png'))
         self.tray.show()
+        
         self.login = Login()
+        if self.config['browser']['cookies']:
+            print self.config['browser']['cookies']
+            cookies = []
+            for name in self.config['browser']['cookies']:
+                cookies.append(
+                    QNetworkCookie(name, self.config['browser']['cookies'][name])
+                )
+            self.login.page().networkAccessManager().cookieJar().setCookiesFromUrl(cookies, QUrl(self.login.base_link))
+        
         self.settings = Settings()
         #--- Connections
         self.login.logged_in.connect(self.on_logged_in)
@@ -24,14 +47,24 @@ class App(QObject):
         self.settings.ui.sync_button.clicked.connect(self.on_sync_button_clicked)
     
     def on_logged_in(self, params):
+        self.config['browser']['cookies'] = {}
+        for cookie in self.login.page().networkAccessManager().cookieJar().cookiesForUrl(QUrl(self.login.base_link)):
+            self.config['browser']['cookies'][cookie.name()] = cookie.value()
+        self.save_settings()
         params_list = params.split('&')
         token = params_list[0].split('=')
         print(token[1])
         self.token = token[1]
-        self.show_settings.emit()
+        if self.config['path']:
+            self.settings.ui.path_to_folder.setText(self.config['path'])
+            self.on_sync_button_clicked()
+        else:
+            self.show_settings.emit()
         
     def on_sync_button_clicked(self):
         self.settings.hide()
+        self.config['path'] = self.settings.ui.path_to_folder.text()
+        self.save_settings()
         self.prg = QProgressDialog(u"Получение списка файлов", u"Отмена", 0, 99)
         self.prg.setWindowTitle("gvkm")
         self.prg.setValue(50)
@@ -51,3 +84,8 @@ class App(QObject):
         
     def on_sync_complete(self):
         self.prg.hide()
+        
+    def save_settings(self):
+        config = open('.config', 'wb')
+        config.write(pickle.dumps(self.config))
+        config.close()
--- Login.py
+++ Login.py
@@ -3,9 +3,9 @@ from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 
 class Login(QWebView):
-    
-    login_link = "http://oauth.vk.com/authorize?client_id=%s&scope=%s&redirect_uri=%s&display=popup&response_type=token"
-    redirect_link = "http://oauth.vk.com/blank.html"
+    base_link = "http://oauth.vk.com/"
+    login_link = "%sauthorize?client_id=%s&scope=%s&redirect_uri=%s&display=popup&response_type=token"
+    redirect_link = "%sblank.html"
     app_id = "2930771"
     scope = "audio"
     logged_in = pyqtSignal(QString)
@@ -19,7 +19,7 @@ class Login(QWebView):
     def login(self):
         self.load(
             QUrl(
-                self.login_link % (self.app_id, self.scope, self.redirect_link)
+                self.login_link % (self.base_link, self.app_id, self.scope, self.redirect_link % self.base_link)
             )
         )
     
@@ -27,7 +27,7 @@ class Login(QWebView):
         if ok:
             url_parts = self.url().toString().split('#')
             if len(url_parts) > 1:
-                if url_parts[0] == self.redirect_link:
+                if url_parts[0] == self.redirect_link % self.base_link:
                     self.hide()
                     self.logged_in.emit(url_parts[1])
                     return
