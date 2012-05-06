cba6ce658fb7a2557bb759863f10dcc776b7508a
Алексей Гомза <gomzik000@gmail.com>
2012-05-06 23:52:06 +0300

    Application not closed, when sync done. SyncThread in other file now

--- App.py
+++ App.py
@@ -7,34 +7,7 @@ import urllib2, json
 import sys, os
 from Login import Login
 from Settings import Settings
-
-class SyncThread(QThread):
-    
-    api_link = "https://api.vk.com/method/"
-    
-    change_status = pyqtSignal(QString, int)
-    sync_complete = pyqtSignal()
-    
-    def __init__(self, token, path, parent = None):
-        QThread.__init__(self, parent)
-        self.token = token
-        self.path = path
-
-    def run(self):
-        response = urllib2.urlopen("%saudio.get?access_token=%s" % (self.api_link, self.token), "r")
-        audios = json.loads(response.read())
-        audios = audios['response']
-        i = 1
-        for audio in audios:
-            self.change_status.emit(u"Скачивание файла %d \ %d" % (i, len(audios)), round(float(i) / float(len(audios)) * 100))
-            filename = "%s/%s - %s.mp3" % (self.path, audio['artist'][:25], audio['title'][:25])
-            if not os.path.exists(filename):
-                mp3 = open(filename,'wb')
-                remote_mp3 = urllib2.urlopen(audio['url'])
-                mp3.write(remote_mp3.read())
-                mp3.close()
-            i += 1
-        self.sync_complete.emit()
+from SyncThread import SyncThread
 
 class App(QObject):
     
@@ -78,4 +51,4 @@ class App(QObject):
         self.prg.setValue(progress)
         
     def on_sync_complete(self):
-        sys.exit()
+        self.prg.hide()
---
+++ SyncThread.py
@@ -0,0 +1,32 @@
+#-*- coding: utf-8 -*-
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class SyncThread(QThread):
+    
+    api_link = "https://api.vk.com/method/"
+    
+    change_status = pyqtSignal(QString, int)
+    sync_complete = pyqtSignal()
+    
+    def __init__(self, token, path, parent = None):
+        QThread.__init__(self, parent)
+        self.token = token
+        self.path = path
+
+    def run(self):
+        response = urllib2.urlopen("%saudio.get?access_token=%s" % (self.api_link, self.token), "r")
+        audios = json.loads(response.read())
+        audios = audios['response']
+        i = 1
+        for audio in audios:
+            self.change_status.emit(u"Скачивание файла %d \ %d" % (i, len(audios)), round(float(i) / float(len(audios)) * 100))
+            filename = "%s/%s - %s.mp3" % (self.path, audio['artist'][:25], audio['title'][:25])
+            if not os.path.exists(filename):
+                mp3 = open(filename,'wb')
+                remote_mp3 = urllib2.urlopen(audio['url'])
+                mp3.write(remote_mp3.read())
+                mp3.close()
+            i += 1
+        self.sync_complete.emit()
